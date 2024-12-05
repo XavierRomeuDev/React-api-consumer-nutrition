8f4cd4136d7ac1690f719dcb644fcba1e9a34afe
Xavier Romeu <xaviermromeu@gmail.com>
2024-12-05 11:53:31 +0100

    remove secrets

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCImrDiYzCRPMpN2GOyYdxEvrrczhHTAC0",
-  authDomain: "practica2-1c78c.firebaseapp.com",
-  databaseURL: "https://practica2-1c78c-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "practica2-1c78c",
-  storageBucket: "practica2-1c78c.appspot.com",
-  messagingSenderId: "282410030421",
-  appId: "1:282410030421:web:59eeddcb3723c90e3d0bdc",
-  measurementId: "G-8CWWG6MD0B"
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId"
 };
 
 const fb = firebase.initializeApp(firebaseConfig);
